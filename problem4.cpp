1a323b1f10589f5a3e31a9fedde6dabe8e7033bf
umid1325 <u.mamasaliyev@newuu.uz>
2024-09-12 04:58:17 -0500

    edit4

--- problem4.cpp
+++ problem4.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 int main() {
     int N;
-    cout<<"Enter the number of students: "<<endl;
     cin>>N;
-    cout<<"The number of free students: "<<endl;
-    cout<<N%50;
+    cout<<50-N%50;
     return 0;
 }
