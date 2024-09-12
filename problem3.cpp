b1c676acae78d2be5194936a40c34d002b4fdbbd
umid1325 <u.mamasaliyev@newuu.uz>
2024-09-12 04:54:06 -0500

    edit3

--- problem3.cpp
+++ problem3.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main() {
     int a;
-    cout<<"Enter the temperature in Celsius:";
     cin>>a;
-    cout<<(a*9/5)+32;
+    cout<<"The temperature in Farenheit is equal to "<<(float(a)*9/5)+32;
     return 0;
 }
